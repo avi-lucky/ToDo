21ffb786be4db0ba7b34bd4bdf269b95eb6181c2
Avikal Shukla <shuklaavikal7@gmail.com>
2021-08-06 19:39:18 +0530

    Forgot Password Created

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express')
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 const auth = require('../middleware/auth');
 const router = new express.Router()
 
@@ -69,25 +70,18 @@ router.post('/users/logout', auth, async (req, res) => {
 router.patch('/users/forgot', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['password']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    // if (!isValidOperation) {
-    //     return res.status(400).send({ error: 'Invalid updates!' })
-    // }
-    const { email } = req.body
-    const user = await User.findOne({ email })
-    console.log(req.body.email)
-    console.log(req.body)
-    console.log(email)
     try {
-        // req.password 
-        // updates.forEach((update) => req.user[update] = req.body[update])
-        await req.user.save()
-        res.send(req.user)
+        const hash = bcrypt.hashSync(req.body.password, 8)
+        // console.log(req.body.email)
+        // console.log(req.body.password)
+        await User.updateOne({email: req.body.email}, {password: hash})
+        const user  = await User.findOne({email: req.body.email})
+        res.status(200).send(user)
         if (!user)
         return res.status(400).send("User With Given Email Doesn't Exist!")
      } catch (e) {
-            res.status(500).send("Error")
+            console.log(e)
+            res.status(500).send(e)
         }
 })
 
